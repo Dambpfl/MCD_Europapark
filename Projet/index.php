005f862ce66a13a457714d25bcb439c5c6f185c8
Damien <boenapfel.damien@yahoo.fr>
2025-01-05 19:14:14 +0100

    Page de base -> listAttractions

--- Projet/index.php
+++ Projet/index.php
@@ -8,14 +8,18 @@ spl_autoload_register(function ($class_name) {
 
 $ctrlEuropapark = new EuropaparkController();
 
+// verifie et recupere l'id via l'url
 $id = isset($_GET['id']) ? (int)$_GET['id'] : null;
 
-if(isset($_GET["action"])){
-    switch ($_GET["action"]) {
+// listAttractions = page de base
+$action = isset($_GET['action']) ? $_GET['action'] : 'listAttractions';
 
-        case "listAttractions" : $ctrlEuropapark->listAttractions(); break;
-        case "listAvis" :  $ctrlEuropapark->listAvis($id); break;
-    }
+switch ($action) {
+
+    case "listAttractions" : $ctrlEuropapark->listAttractions(); break;
+    case "listAvis" :  $ctrlEuropapark->listAvis($id); break;
+
 }
+
 
 ?>
